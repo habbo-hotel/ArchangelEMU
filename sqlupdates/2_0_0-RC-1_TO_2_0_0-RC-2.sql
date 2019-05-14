3b480db9f6ec3ef71a5d8461d617e1999fd39fce
Harmony <git@nextgenhabbo.com>
2019-05-14 20:14:15 +0100

    Make plugins window style configurable between default and the old scrollable style

---
+++ sqlupdates/2_0_0-RC-1_TO_2_0_0-RC-2.sql
@@ -0,0 +1,5 @@
+#DATABASE UPDATE: 2.0.0 RC-2 -> 2.0.0 RC-3
+
+INSERT INTO `emulator_settings` (`key`, `value`) VALUES ('commands.plugins.oldstyle', '0');
+
+#END DATABASE UPDATE: 2.0.0 RC-2 -> 2.0.0 RC-3
