9b15464fd1219aab3be4d66dd4cea24e4aaf1f9c
Beny <ben@beny.pro>
2019-05-16 13:26:05 +0100

    Removed whitespace

--- sqlupdates/2_0_0-RC-2_TO_2_0_0-RC-3.sql
+++ sqlupdates/2_0_0-RC-2_TO_2_0_0-RC-3.sql
@@ -1,11 +1,11 @@
 #DATABASE UPDATE: 2.0.0 RC-2 -> 2.0.0 RC-3
 
-                                                                                                                                          INSERT INTO `emulator_settings` (`key`, `value`) VALUES ('commands.plugins.oldstyle', '0');
+INSERT INTO `emulator_settings` (`key`, `value`) VALUES ('commands.plugins.oldstyle', '0');
 
-                                                                                                                                          ALTER TABLE `emulator_errors`
-                                                                                                                                          ADD COLUMN `version`  varchar(64) NOT NULL AFTER `timestamp`,
-                                                                                                                                          ADD COLUMN `build_hash`  varchar(64) NOT NULL AFTER `version`;
+ALTER TABLE `emulator_errors`
+ADD COLUMN `version`  varchar(64) NOT NULL AFTER `timestamp`,
+ADD COLUMN `build_hash`  varchar(64) NOT NULL AFTER `version`;
 
-                                                                                                                                          INSERT INTO `emulator_settings`(`key`, `value`) VALUES ('scripter.modtool.tickets', '1');
+INSERT INTO `emulator_settings`(`key`, `value`) VALUES ('scripter.modtool.tickets', '1');
 
-                                                                                                                                          #END DATABASE UPDATE: 2.0.0 RC-2 -> 2.0.0 RC-3
+#END DATABASE UPDATE: 2.0.0 RC-2 -> 2.0.0 RC-3
