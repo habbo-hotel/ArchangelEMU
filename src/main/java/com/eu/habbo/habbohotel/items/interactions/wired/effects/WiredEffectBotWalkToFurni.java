75985a3696802abba07805b64198baf1d8925f6a
Beny <ben@beny.pro>
2019-05-18 14:14:58 +0100

    Fixed a IllegalArgumentException in WiredEffectBotWalkToFurni

--- src/main/java/com/eu/habbo/habbohotel/items/interactions/wired/effects/WiredEffectBotWalkToFurni.java
+++ src/main/java/com/eu/habbo/habbohotel/items/interactions/wired/effects/WiredEffectBotWalkToFurni.java
@@ -119,21 +119,18 @@ public class WiredEffectBotWalkToFurni extends InteractionWiredEffect
             this.items.remove(item);
         }
 
-        for(Bot bot : bots)
-        {
-            int i = Emulator.getRandom().nextInt(this.items.size()) + 1;
-            int j = 1;
-            for (HabboItem item : this.items)
-            {
-                if(item.getRoomId() != 0 && item.getRoomId() == bot.getRoom().getId())
-                {
-                    if (i == j)
-                    {
-                        bot.getRoomUnit().setGoalLocation(room.getLayout().getTile(item.getX(), item.getY()));
-                        break;
-                    } else
-                    {
-                        j++;
+        if(this.items.size() > 0) {
+            for (Bot bot : bots) {
+                int i = Emulator.getRandom().nextInt(this.items.size()) + 1;
+                int j = 1;
+                for (HabboItem item : this.items) {
+                    if (item.getRoomId() != 0 && item.getRoomId() == bot.getRoom().getId()) {
+                        if (i == j) {
+                            bot.getRoomUnit().setGoalLocation(room.getLayout().getTile(item.getX(), item.getY()));
+                            break;
+                        } else {
+                            j++;
+                        }
                     }
                 }
             }
