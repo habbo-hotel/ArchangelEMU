ccca21c171690c66b305012fb7860f1a4bd3fe9a
Alejandro <25-alejandro@users.noreply.git.krews.org>
2019-05-26 23:15:16 +0300

    Add forum moderation achievements

--- src/main/java/com/eu/habbo/messages/incoming/users/UserActivityEvent.java
+++ src/main/java/com/eu/habbo/messages/incoming/users/UserActivityEvent.java
@@ -9,12 +9,28 @@ public class UserActivityEvent extends MessageHandler {
     public void handle() throws Exception {
         String type = this.packet.readString();
         String value = this.packet.readString();
+        String action = this.packet.readString();
 
         switch (type) {
             case "Quiz":
                 if (value.equalsIgnoreCase("7")) {
                     AchievementManager.progressAchievement(this.client.getHabbo(), Emulator.getGameEnvironment().getAchievementManager().getAchievement("SafetyQuizGraduate"));
                 }
+        }
+
+        switch (action) {
+            case "forum.can.read.seen":
+                AchievementManager.progressAchievement(this.client.getHabbo(), Emulator.getGameEnvironment().getAchievementManager().getAchievement("SelfModForumCanReadSeen"));
+                break;
+            case "forum.can.post.seen":
+                AchievementManager.progressAchievement(this.client.getHabbo(), Emulator.getGameEnvironment().getAchievementManager().getAchievement("SelfModForumCanPostSeen"));
+                break;
+            case "forum.can.start.thread.seen":
+                AchievementManager.progressAchievement(this.client.getHabbo(), Emulator.getGameEnvironment().getAchievementManager().getAchievement("SelfModForumCanPostThrdSeen"));
+                break;
+            case "forum.can.moderate.seen":
+                AchievementManager.progressAchievement(this.client.getHabbo(), Emulator.getGameEnvironment().getAchievementManager().getAchievement("SelfModForumCanModerateSeen"));
+                break;
         }
     }
 }
