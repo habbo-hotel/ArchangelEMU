8aae182f6a3131cbd214a1757ed60fa3499fc79d
Alejandro <25-alejandro@users.noreply.git.krews.org>
2019-05-27 00:43:14 +0300

    Add RbBunnyTag achievement

--- src/main/java/com/eu/habbo/habbohotel/items/interactions/games/tag/bunnyrun/InteractionBunnyrunField.java
+++ src/main/java/com/eu/habbo/habbohotel/items/interactions/games/tag/bunnyrun/InteractionBunnyrunField.java
@@ -1,8 +1,13 @@
 package com.eu.habbo.habbohotel.items.interactions.games.tag.bunnyrun;
 
+import com.eu.habbo.Emulator;
+import com.eu.habbo.habbohotel.achievements.Achievement;
+import com.eu.habbo.habbohotel.achievements.AchievementManager;
 import com.eu.habbo.habbohotel.games.tag.BunnyrunGame;
 import com.eu.habbo.habbohotel.items.Item;
 import com.eu.habbo.habbohotel.items.interactions.games.tag.InteractionTagField;
+import com.eu.habbo.habbohotel.rooms.Room;
+import com.eu.habbo.habbohotel.users.Habbo;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -14,5 +19,16 @@ public class InteractionBunnyrunField extends InteractionTagField {
 
     public InteractionBunnyrunField(int id, int userId, Item item, String extradata, int limitedStack, int limitedSells) {
         super(id, userId, item, extradata, limitedStack, limitedSells, BunnyrunGame.class);
+    }
+
+    @Override
+    public void onPlace(Room room) {
+        super.onPlace(room);
+
+        Habbo itemOwner = Emulator.getGameEnvironment().getHabboManager().getHabbo(this.getUserId());
+
+        if (itemOwner != null) {
+            AchievementManager.progressAchievement(itemOwner, Emulator.getGameEnvironment().getAchievementManager().getAchievement("RbBunnyTag"));
+        }
     }
 }
