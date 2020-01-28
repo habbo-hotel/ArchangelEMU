42ebfad128175bb822a32052dd99c0cb9b4f05d4
Alejandro <25-alejandro@users.noreply.git.krews.org>
2020-01-28 15:13:32 +0200

    Fix moving/rotatingg furniture in the same coordinates (closes #352)

--- src/main/java/com/eu/habbo/habbohotel/rooms/Room.java
+++ src/main/java/com/eu/habbo/habbohotel/rooms/Room.java
@@ -4486,8 +4486,11 @@ public class Room implements Comparable<Room>, ISerialize, Runnable {
             }
         }
         //Place at new position
+
         double height = this.getStackHeight(tile.x, tile.y, false, item);
+        if (tile.x == item.getX() && tile.y == item.getY()) height = item.getZ();
         if(height > 40d) return FurnitureMovementError.CANT_STACK;
+
         item.setX(tile.x);
         item.setY(tile.y);
         item.setZ(height);
