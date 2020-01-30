fcccd70eb310b96d8cf8956b6685e24d78ccad47
skeletor <torresefrain10@gmail.com>
2020-01-30 03:29:01 -0500

    tent should not change state when clicked

--- src/main/java/com/eu/habbo/habbohotel/items/interactions/InteractionTent.java
+++ src/main/java/com/eu/habbo/habbohotel/items/interactions/InteractionTent.java
@@ -1,6 +1,8 @@
 package com.eu.habbo.habbohotel.items.interactions;
 
+import com.eu.habbo.habbohotel.gameclients.GameClient;
 import com.eu.habbo.habbohotel.items.Item;
+import com.eu.habbo.habbohotel.rooms.Room;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -12,5 +14,10 @@ public class InteractionTent extends InteractionDefault {
 
     public InteractionTent(int id, int userId, Item item, String extradata, int limitedStack, int limitedSells) {
         super(id, userId, item, extradata, limitedStack, limitedSells);
+    }
+
+    @Override
+    public void onClick(GameClient client, Room room, Object[] objects) throws Exception {
+        // do nothing
     }
 }
