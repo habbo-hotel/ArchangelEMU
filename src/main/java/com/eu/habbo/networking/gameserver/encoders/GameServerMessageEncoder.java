a93ffbbbaca053c9c7ce8b8986ef3969afebc4ac
Mike <76-Mike@users.noreply.git.krews.org>
2020-05-09 22:41:43 +0200

    Added debug.

--- src/main/java/com/eu/habbo/networking/gameserver/encoders/GameServerMessageEncoder.java
+++ src/main/java/com/eu/habbo/networking/gameserver/encoders/GameServerMessageEncoder.java
@@ -4,18 +4,28 @@ import com.eu.habbo.messages.ServerMessage;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToByteEncoder;
+import io.netty.util.IllegalReferenceCountException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class GameServerMessageEncoder extends MessageToByteEncoder<ServerMessage> {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(GameServerMessageEncoder.class);
+
     @Override
     protected void encode(ChannelHandlerContext ctx, ServerMessage message, ByteBuf out) {
-        ByteBuf buf = message.get();
-
         try {
-            out.writeBytes(buf);
-        } finally {
-            // Release copied buffer.
-            buf.release();
+            ByteBuf buf = message.get();
+
+            try {
+                out.writeBytes(buf);
+            } finally {
+                // Release copied buffer.
+                buf.release();
+            }
+        } catch (IllegalReferenceCountException e) {
+            LOGGER.error("IllegalReferenceCountException happened for packet {}.", message.getHeader());
+            throw e;
         }
     }
 
