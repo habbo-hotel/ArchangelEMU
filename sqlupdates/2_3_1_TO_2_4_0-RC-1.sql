95fab253d8b6043e794a01dc4b667ab1b1300cec
harmonic <harmonicrain2016@gmail.com>
2020-05-09 23:51:56 +0100

    Reverted 2.4.0 sql.

--- sqlupdates/2_3_1_TO_2_4_0-RC-1.sql
+++ sqlupdates/2_3_1_TO_2_4_0-RC-1.sql
@@ -1,5 +1,3 @@
 INSERT INTO `emulator_settings`(`key`, `value`) VALUES ('hotel.room.stickies.max', '200');
 
-ALTER TABLE `users_settings` ADD COLUMN `last_purchase_timestamp` int(11) NOT NULL DEFAULT UNIX_TIMESTAMP();
-
 INSERT INTO `emulator_settings`(`key`, `value`) VALUES ('retro.style.homeroom', '1');
