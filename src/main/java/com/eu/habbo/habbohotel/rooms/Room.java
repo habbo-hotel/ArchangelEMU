8fb71247ab3179a72889c7fcc97b12171751a54e
Harmonic <johnjamesharding@hotmail.co.uk>
2020-07-31 16:03:04 -0400

    Revert "Merge branch 'wired-spam' into 'dev'"
    This reverts merge request !266

--- src/main/java/com/eu/habbo/habbohotel/rooms/Room.java
+++ src/main/java/com/eu/habbo/habbohotel/rooms/Room.java
@@ -3005,7 +3005,7 @@ public class Room implements Comparable<Room>, ISerialize, Runnable {
     }
 
     public void talk(Habbo habbo, RoomChatMessage roomChatMessage, RoomChatType chatType) {
-        this.talk(habbo, roomChatMessage, chatType, true);
+        this.talk(habbo, roomChatMessage, chatType, false);
     }
 
     public void talk(final Habbo habbo, final RoomChatMessage roomChatMessage, RoomChatType chatType, boolean ignoreWired) {
