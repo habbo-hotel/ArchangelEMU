840dd426d3bc83dbaf5ff2de9c83e1c4f55d38a3
Beny <beny@krews.org>
2020-11-15 22:43:32 +0100

    Fix trophies breaking wired

--- src/main/java/com/eu/habbo/habbohotel/items/interactions/InteractionTrophy.java
+++ src/main/java/com/eu/habbo/habbohotel/items/interactions/InteractionTrophy.java
@@ -13,4 +13,9 @@ public class InteractionTrophy extends InteractionDefault {
     public InteractionTrophy(int id, int userId, Item item, String extradata, int limitedStack, int limitedSells) {
         super(id, userId, item, extradata, limitedStack, limitedSells);
     }
+
+    @Override
+    public boolean allowWiredResetState() {
+        return false;
+    }
 }
