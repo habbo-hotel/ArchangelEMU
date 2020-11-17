a72ebee60329bdd7f938afa703b5bd76d08ea7e8
ArpyAge <ricardo_hoek@hotmail.com>
2020-11-17 21:15:37 +0100

    Furniture Redeemed Event point type fix

--- src/main/java/com/eu/habbo/plugin/events/furniture/FurnitureRedeemedEvent.java
+++ src/main/java/com/eu/habbo/plugin/events/furniture/FurnitureRedeemedEvent.java
@@ -4,16 +4,12 @@ import com.eu.habbo.habbohotel.users.Habbo;
 import com.eu.habbo.habbohotel.users.HabboItem;
 
 public class FurnitureRedeemedEvent extends FurnitureUserEvent {
-    public static final int CREDITS = 0;
-    public static final int PIXELS = 1;
+    public static final int CREDITS = -1;
+    public static final int PIXELS = 0;
     public static final int DIAMONDS = 5;
 
-
     public final int amount;
-
-
     public final int currencyID;
-
 
     public FurnitureRedeemedEvent(HabboItem furniture, Habbo habbo, int amount, int currencyID) {
         super(furniture, habbo);
