5720adf61c7ed426321cbba87755735c484d4049
Remco <habboilany@gmail.com>
2021-01-04 18:54:59 +0100

    WiredEffectMoveFurniTowards now saves as json import fix

--- src/main/java/com/eu/habbo/habbohotel/items/interactions/wired/effects/WiredEffectMoveFurniTowards.java
+++ src/main/java/com/eu/habbo/habbohotel/items/interactions/wired/effects/WiredEffectMoveFurniTowards.java
@@ -22,6 +22,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Wired effect: move to closest user
