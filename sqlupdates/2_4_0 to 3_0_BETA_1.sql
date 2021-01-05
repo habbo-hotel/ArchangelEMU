d6ecdbeeecb992bf6cc9aacdb215b091f02e359b
Remco <habboilany@gmail.com>
2021-01-05 21:55:23 +0100

    Added SQL to expand items.wired_data column

--- sqlupdates/2_4_0 to 3_0_BETA_1.sql
+++ sqlupdates/2_4_0 to 3_0_BETA_1.sql
@@ -114,3 +114,6 @@ ADD COLUMN `bubble_id`  int(3) NULL DEFAULT 31 AFTER `effect`;
 -- Permissions to see tent chat
 ALTER TABLE `permissions` ADD `acc_see_tentchat` ENUM('0', '1') NOT NULL DEFAULT '0' AFTER `acc_see_whispers`;
 INSERT INTO `emulator_texts` (`key`, `value`) VALUES ('hotel.room.tent.prefix', 'Tent');
+
+-- Making items.wired_data column bigger since wired data is saved as JSON now
+ALTER TABLE `items` MODIFY COLUMN `wired_data` varchar(10000)
