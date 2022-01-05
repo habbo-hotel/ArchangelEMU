f99de2009cc4caf66b74a495154e8b521288064f
SenpaiDipper <amedbarrawi@gmail.com>
2022-01-05 19:42:04 +0000

    Yikes Sorry.

--- src/main/java/com/eu/habbo/habbohotel/commands/SoftKickCommand.java
+++ src/main/java/com/eu/habbo/habbohotel/commands/SoftKickCommand.java
@@ -2,6 +2,7 @@ package com.eu.habbo.habbohotel.commands;
 
 import com.eu.habbo.Emulator;
 import com.eu.habbo.habbohotel.gameclients.GameClient;
+import com.eu.habbo.habbohotel.permissions.Permission;
 import com.eu.habbo.habbohotel.rooms.Room;
 import com.eu.habbo.habbohotel.rooms.RoomChatMessageBubbles;
 import com.eu.habbo.habbohotel.users.Habbo;
@@ -30,7 +31,9 @@ public class SoftKickCommand extends Command {
             final Room room = gameClient.getHabbo().getHabboInfo().getCurrentRoom();
 
             if (room != null) {
-                room.kickHabbo(habbo, false);
+                if (!(habbo.hasPermission(Permission.ACC_UNKICKABLE) || habbo.hasPermission(Permission.ACC_SUPPORTTOOL) || room.isOwner(habbo))) {
+                    room.kickHabbo(habbo, false);
+                }
             }
         }
         return true;
