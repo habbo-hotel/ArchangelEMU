5d82a6e42653ce044d0853f095569b28c8601869
dank074 <torresefrain10@gmail.com>
2022-03-18 17:26:43 -0500

    remove forced room forwarding to home room

--- src/main/java/com/eu/habbo/messages/incoming/users/RequestUserDataEvent.java
+++ src/main/java/com/eu/habbo/messages/incoming/users/RequestUserDataEvent.java
@@ -43,11 +43,6 @@ public class RequestUserDataEvent extends MessageHandler {
             messages.add(new UserDataComposer(this.client.getHabbo()).compose());
             messages.add(new UserPerksComposer(this.client.getHabbo()).compose());
 
-            if (!this.client.getHabbo().getHabboStats().nux || Emulator.getConfig().getBoolean("retro.style.homeroom") && this.client.getHabbo().getHabboInfo().getHomeRoom() != 0)
-                messages.add(new ForwardToRoomComposer(this.client.getHabbo().getHabboInfo().getHomeRoom()).compose());
-            else   if (!this.client.getHabbo().getHabboStats().nux || Emulator.getConfig().getBoolean("retro.style.homeroom") && RoomManager.HOME_ROOM_ID > 0)
-                messages.add(new ForwardToRoomComposer(RoomManager.HOME_ROOM_ID).compose());
-
             messages.add(new MeMenuSettingsComposer(this.client.getHabbo()).compose());
 
 
