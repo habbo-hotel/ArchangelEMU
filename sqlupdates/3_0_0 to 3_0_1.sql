5a66c30e8dc6cea4de1557b4584b93fcb7335646
brenoepics <ibreno076@gmail.com>
2022-03-19 19:34:21 +0000

    Feature Friendlist Categories

--- sqlupdates/3_0_0 to 3_0_1.sql
+++ sqlupdates/3_0_0 to 3_0_1.sql
@@ -10,3 +10,14 @@ INSERT INTO `emulator_settings` (`key`, `value`) VALUES ('console.mode', '1');
 INSERT INTO `emulator_settings` (`key`, `value`) VALUES ('youtube.apikey', '');
 
 INSERT INTO `emulator_settings` (`key`, `value`) VALUES ('hotel.gifts.length.max', '300');
+
+-- Add friendship categories table
+CREATE TABLE `messenger_categories` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `name` varchar(25) NOT NULL,
+    `user_id` int NOT NULL,
+    UNIQUE KEY `identifier` (`id`)
+);
+
+-- Set an ID (int) from category list items
+ALTER TABLE messenger_friendships ADD category int NOT NULL DEFAULT '0' AFTER friends_since;
