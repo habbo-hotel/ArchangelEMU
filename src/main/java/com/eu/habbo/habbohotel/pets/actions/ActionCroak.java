e3f4d2898c4663a821759842a2eacd94626a4164
Oliver <oliver321@cloudhotelli.net>
2022-04-11 13:00:26 +0000

    Fix ActionCroak

--- src/main/java/com/eu/habbo/habbohotel/pets/actions/ActionCroak.java
+++ src/main/java/com/eu/habbo/habbohotel/pets/actions/ActionCroak.java
@@ -21,8 +21,18 @@ public class ActionCroak extends PetAction {
 
         Emulator.getThreading().run(new PetClearPosture(pet, RoomUnitStatus.CROAK, null, false), 2000);
 
-        if (pet.getHappyness() > 80)
-            pet.say(pet.getPetData().randomVocal(PetVocalsType.PLAYFUL));
+        if (pet.getHappyness() > 70)
+            pet.say(pet.getPetData().randomVocal(PetVocalsType.GENERIC_HAPPY));
+        else if (pet.getHappyness() < 30)
+            pet.say(pet.getPetData().randomVocal(PetVocalsType.GENERIC_SAD));
+        else if (pet.getLevelHunger() > 65)
+            pet.say(pet.getPetData().randomVocal(PetVocalsType.HUNGRY));
+        else if (pet.getLevelThirst() > 65)
+            pet.say(pet.getPetData().randomVocal(PetVocalsType.THIRSTY));
+        else if (pet.getEnergy() < 25)
+            pet.say(pet.getPetData().randomVocal(PetVocalsType.TIRED));
+        else if (pet.getTask() == PetTasks.NEST || pet.getTask() == PetTasks.DOWN)
+            pet.say(pet.getPetData().randomVocal(PetVocalsType.SLEEPING));
 
         return true;
     }
