ab5c491aa54f5d3a27bf65a92d337199465c92c4
brenoepic <59066707+brenoepics@users.noreply.github.com>
2022-04-12 23:03:48 -0300

    Fix mannequin name

--- sqlupdates/3_0_0 to 3_0_1.sql
+++ sqlupdates/3_0_0 to 3_0_1.sql
@@ -85,3 +85,6 @@ INSERT INTO `emulator_texts` (`key`, `value`) VALUES ('commands.success.cmd_upda
 
 -- add moodlight configuration
 INSERT INTO `emulator_settings` (`key`, `value`) VALUES ('moodlight.color_check.enabled', '1');
+
+-- Mannequin name
+INSERT INTO `emulator_texts` (`key`, `value`) VALUES ('hotel.mannequin.name.default', 'My look');
