023183a647d19fb310f81f8f5405e34af86376a9
brenoepic <59066707+brenoepics@users.noreply.github.com>
2022-04-29 19:32:51 -0300

    Fix CallForHelpEvent

--- src/main/java/com/eu/habbo/messages/incoming/modtool/CallForHelpFromIMEvent.java
+++ src/main/java/com/eu/habbo/messages/incoming/modtool/CallForHelpFromIMEvent.java
@@ -35,14 +35,14 @@ public class CallForHelpFromIMEvent extends MessageHandler {
             info = HabboManager.getOfflineHabboInfo(userId);
         }
 
-        if (info != null) {
-            for (int i = 0; i < count; i++) {
-                int chatUserId = this.packet.readInt();
-                String username = this.packet.readInt() == info.getId() ? info.getUsername() : this.client.getHabbo().getHabboInfo().getUsername();
+        if (info == null) return;
+        for (int i = 0; i < count; i++) {
+            int chatUserId = this.packet.readInt();
+            String username = this.packet.readInt() == info.getId() ? info.getUsername() : this.client.getHabbo().getHabboInfo().getUsername();
 
-                chatLogs.add(new ModToolChatLog(0, chatUserId, username, this.packet.readString()));
-            }
+            chatLogs.add(new ModToolChatLog(0, chatUserId, username, this.packet.readString()));
         }
+
 
         ModToolIssue issue = new ModToolIssue(this.client.getHabbo().getHabboInfo().getId(), this.client.getHabbo().getHabboInfo().getUsername(), userId, info.getUsername(), 0, message, ModToolTicketType.IM);
         issue.category = category;
