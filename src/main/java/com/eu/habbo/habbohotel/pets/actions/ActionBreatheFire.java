e681e04f3d924cc5651437f3626927c14f4da8bc
Oliver <oliver321@cloudhotelli.net>
2022-05-02 23:28:54 +0000

    Update ActionBreatheFire.java

--- src/main/java/com/eu/habbo/habbohotel/pets/actions/ActionBreatheFire.java
+++ src/main/java/com/eu/habbo/habbohotel/pets/actions/ActionBreatheFire.java
@@ -19,8 +19,11 @@ public class ActionBreatheFire extends PetAction {
     public boolean apply(Pet pet, Habbo habbo, String[] data) {
         Emulator.getThreading().run(new PetClearPosture(pet, RoomUnitStatus.FLAME, null, false), this.minimumActionDuration);
 
-        if (pet.getHappyness() > 50)
+        if (pet.getHappyness() > 80) {
             pet.say(pet.getPetData().randomVocal(PetVocalsType.PLAYFUL));
+        } else {
+            pet.say(pet.getPetData().randomVocal(PetVocalsType.GENERIC_NEUTRAL));
+        }
 
         return true;
     }
