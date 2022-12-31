7f3521f8218b5e80c8a0cb85c00c7b27248f7620
brenoepic <git@brenoepic.tech>
2022-12-31 07:29:45 +0000

    Update 3_5_0 to 4_0_0.sql

--- sqlupdates/3_5_0 to 4_0_0.sql
+++ sqlupdates/3_5_0 to 4_0_0.sql
@@ -4,3 +4,5 @@ INSERT INTO `emulator_texts` (`key`, `value`) VALUES ('commands.succes.cmd_chang
 
 ALTER TABLE `achievements`
 ADD `visible` enum('0','1') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '1' COMMENT 'Is the achievement in use and/or obtainable?';
+
+INSERT INTO `emulator_settings` (`key`, `value`) VALUES ('encryption.forced', '0');
