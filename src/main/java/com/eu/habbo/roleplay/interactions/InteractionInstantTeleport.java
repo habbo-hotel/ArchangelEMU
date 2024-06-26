ac0dccbe60caf515a9dc98fd53252a40c89fe144
chrismpettyjohn <chrismpettyjohn@gmail.com>
2024-06-26 17:13:32 -0400

    fix: code cleanup

--- src/main/java/com/eu/habbo/roleplay/interactions/InteractionInstantTeleport.java
+++ src/main/java/com/eu/habbo/roleplay/interactions/InteractionInstantTeleport.java
@@ -1,14 +1,12 @@
 package com.eu.habbo.roleplay.interactions;
 
 import com.eu.habbo.Emulator;
-import com.eu.habbo.habbohotel.gameclients.GameClient;
 import com.eu.habbo.habbohotel.items.Item;
 import com.eu.habbo.habbohotel.items.interactions.InteractionTeleport;
 import com.eu.habbo.habbohotel.rooms.Room;
 import com.eu.habbo.habbohotel.rooms.entities.units.RoomUnit;
 import com.eu.habbo.habbohotel.users.Habbo;
 import com.eu.habbo.habbohotel.users.HabboInfo;
-import com.eu.habbo.roleplay.facility.hospital.FacilityHospitalManager;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -18,7 +16,6 @@ public class InteractionInstantTeleport extends InteractionTeleport {
     public static String INTERACTION_TYPE = "rp_instant_teleport";
 
     private Habbo habbo;
-
 
     public InteractionInstantTeleport(ResultSet set, Item baseItem) throws SQLException {
         super(set, baseItem);
@@ -50,10 +47,5 @@ public class InteractionInstantTeleport extends InteractionTeleport {
         Emulator.getThreading().run(() -> {
             this.habbo.shout(Emulator.getTexts().getValue("roleplay.teleport.arrived"));
         },  1500);
-    }
-
-    @Override
-    public void onClick(GameClient client, Room room, Object[] objects) {
-
     }
 }
