ce544a03084c749932d3ca27375afa17c370402e
chrismpettyjohn <chrismpettyjohn@gmail.com>
2024-06-29 21:59:16 -0400

    feat: add ability to clock out early

--- src/main/java/com/eu/habbo/roleplay/actions/WorkShiftAction.java
+++ src/main/java/com/eu/habbo/roleplay/actions/WorkShiftAction.java
@@ -10,79 +10,83 @@ import com.eu.habbo.roleplay.corp.CorpPosition;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 public class WorkShiftAction implements Runnable {
     private final Habbo habbo;
-    private final long endsAt;
-
-    private final Corp corp;
-    private final CorpPosition corpPosition;
-
-    private final String oldMotto;
-    private final String oldFigure;
+    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(2);
+    private ScheduledFuture<?> cycleUserShift;
+    private ScheduledFuture<?> checkWorkingState;
 
     public WorkShiftAction(Habbo habbo) {
         this.habbo = habbo;
-        this.oldMotto = habbo.getHabboInfo().getMotto();
-        this.oldFigure = habbo.getHabboInfo().getLook();
-        this.corp = habbo.getHabboRoleplayStats().getCorp();
-        this.corpPosition = habbo.getHabboRoleplayStats().getCorpPosition();
-        this.endsAt = Instant.now().getEpochSecond() + Duration.ofMinutes(10).getSeconds();
-
-        this.onShiftStart();
     }
 
     @Override
     public void run() {
+        String oldMotto = habbo.getHabboInfo().getMotto();
+        String oldFigure = habbo.getHabboInfo().getLook();
+        Corp corp = habbo.getHabboRoleplayStats().getCorp();
+        CorpPosition corpPosition = habbo.getHabboRoleplayStats().getCorpPosition();
+        long endsAt = Instant.now().getEpochSecond() + Duration.ofMinutes(10).getSeconds();
+
+        cycleUserShift = scheduler.scheduleAtFixedRate(() -> cycleUserShift(habbo, corpPosition, endsAt), 0, 60, TimeUnit.SECONDS);
+        checkWorkingState = scheduler.scheduleAtFixedRate(() -> checkWorkingState(habbo, corpPosition), 0, 1, TimeUnit.SECONDS);
+
+        habbo.shout(Emulator.getTexts().getValue("roleplay.shift.start").replace(":position", corpPosition.getName()));
+        habbo.getHabboRoleplayStats().setWorking(true);
+        habbo.getHabboInfo().setMotto(corpPosition.getActivity());
+        habbo.getHabboInfo().changeClothes(habbo.getHabboInfo().getGender() == HabboGender.M ? corpPosition.getMaleFigure() : corpPosition.getFemaleFigure());
+        habbo.getClient().sendResponse(new FigureUpdateComposer(habbo));
+        habbo.getRoomUnit().getRoom().sendComposer(new UserChangeMessageComposer(habbo.getClient().getHabbo()).compose());
+    }
+
+    private void checkWorkingState(Habbo habbo, CorpPosition corpPosition) {
+        if (!habbo.getHabboRoleplayStats().isWorking()) {
+            habbo.shout(Emulator.getTexts().getValue("roleplay.shift.cancel"));
+            cycleUserShift.cancel(true);
+            checkWorkingState.cancel(true);
+        }
+    }
+
+    private void cycleUserShift(Habbo habbo, CorpPosition corpPosition, long endsAt) {
         if (habbo.getRoomUnit().getRoom() == null) {
             return;
         }
 
-        if ( (habbo.getRoomUnit().getRoom().getRoomInfo().getId() != this.corp.getGuild().getRoomId()) && !this.corpPosition.isCanWorkAnywhere()) {
-            this.habbo.shout(Emulator.getTexts().getValue("roleplay.shift.cancel"));
+        Corp corp = habbo.getHabboRoleplayStats().getCorp();
+
+        if (habbo.getRoomUnit().getRoom().getRoomInfo().getId() != corp.getGuild().getRoomId() && !corpPosition.isCanWorkAnywhere()) {
+            habbo.shout(Emulator.getTexts().getValue("roleplay.shift.cancel"));
             return;
         }
 
-        if ((System.currentTimeMillis() / 1000) >= this.endsAt) {
-            this.onShiftFinish();
+        if ((System.currentTimeMillis() / 1000) >= endsAt) {
+            onShiftFinish(habbo, corpPosition);
             return;
         }
 
         long currentTimeInSeconds = System.currentTimeMillis() / 1000;
-        int secondsLeft = (int) (this.endsAt - currentTimeInSeconds);
+        int secondsLeft = (int) (endsAt - currentTimeInSeconds);
         int minutesLeft = secondsLeft / 60;
 
-        this.habbo.shout(Emulator.getTexts()
-                .getValue("roleplay.shift.progress")
+        habbo.shout(Emulator.getTexts().getValue("roleplay.shift.progress")
                 .replace(":time", String.valueOf(minutesLeft > 0 ? minutesLeft : secondsLeft))
                 .replace(":unit", minutesLeft > 0 ? "minutes" : "seconds"));
-
-        Emulator.getThreading().run(this, 25000);
     }
 
-    private void onShiftStart() {
-        this.habbo.shout(Emulator.getTexts()
-                .getValue("roleplay.shift.start")
-                .replace(":position", this.corpPosition.getName())
-        );
-        this.habbo.getHabboRoleplayStats().setWorking(true);
-        this.habbo.getHabboInfo().setMotto(this.corpPosition.getActivity());
-        this.habbo.getHabboInfo().changeClothes(this.habbo.getHabboInfo().getGender() == HabboGender.M ? this.corpPosition.getMaleFigure() : this.corpPosition.getFemaleFigure());
-        this.habbo.getClient().sendResponse(new FigureUpdateComposer(this.habbo));
-        this.habbo.getRoomUnit().getRoom().sendComposer(new UserChangeMessageComposer(this.habbo.getClient().getHabbo()).compose());
+    private void onShiftFinish(Habbo habbo, CorpPosition corpPosition) {
+        habbo.shout(Emulator.getTexts().getValue("roleplay.shift.success")
+                .replace(":credits", String.valueOf(corpPosition.getSalary())));
+        habbo.getHabboRoleplayStats().setWorking(false);
+        habbo.getHabboInfo().setCredits(habbo.getHabboInfo().getCredits() + corpPosition.getSalary());
+        habbo.getHabboInfo().setMotto(habbo.getHabboInfo().getMotto());
+        habbo.getHabboInfo().changeClothes(habbo.getHabboInfo().getLook());
+        habbo.getClient().sendResponse(new FigureUpdateComposer(habbo));
+        habbo.getRoomUnit().getRoom().sendComposer(new UserChangeMessageComposer(habbo.getClient().getHabbo()).compose());
     }
-
-    private void onShiftFinish() {
-        this.habbo.shout(Emulator.getTexts()
-                .getValue("roleplay.shift.success")
-                .replace(":credits", String.valueOf(this.corpPosition.getSalary()))
-        );
-        this.habbo.getHabboRoleplayStats().setWorking(false);
-        this.habbo.getHabboInfo().setCredits(this.habbo.getHabboInfo().getCredits() + this.corpPosition.getSalary());
-        this.habbo.getHabboInfo().setMotto(this.oldMotto);
-        this.habbo.getHabboInfo().changeClothes(this.oldFigure);
-        this.habbo.getClient().sendResponse(new FigureUpdateComposer(this.habbo));
-        this.habbo.getRoomUnit().getRoom().sendComposer(new UserChangeMessageComposer(this.habbo.getClient().getHabbo()).compose());
-    }
-
 }
+
